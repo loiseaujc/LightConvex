b053cd752e97b8c8998e83c7b81576ee986b7b35
loiseaujc <loiseau.jc@gmail.com>
2025-09-02 05:54:37 +0000

    Deploy FORD docs

--- API-doc/src/LightConvex.f90
+++ API-doc/src/LightConvex.f90
@@ -12,50 +12,25 @@ module LightConvex
 
    real(dp), parameter :: eps = epsilon(1.0_dp)
 
-   !----------------------------------------------
-   !-----     FUNDAMENTAL ABSTRACT TYPES     -----
-   !----------------------------------------------
-
-   type, abstract :: AbstractConvexProblem
-   end type AbstractConvexProblem
-
-   type, abstract :: AbstractAlgorithm
-   end type AbstractAlgorithm
-
    !--------------------------------------
    !-----     LINEAR PROGRAMMING     -----
    !--------------------------------------
 
-   type, extends(AbstractConvexProblem), public :: lp_type
-      private
-      real(dp), allocatable :: c(:)
-        !! Cost function.
-      real(dp), allocatable :: A(:, :)
-        !! Linear constraints.
-      integer(ilp) :: nleq, ngeq, neq
-        !! Number of constraints of each type.
-   end type lp_type
-
    interface simplex
-      pure module subroutine dense_simplex(A, nleq, ngeq, neq, icase, izrov, iposv)
+      pure module subroutine dense_standard_simplex(A, nleq, ngeq, neq, info, iposv)
          implicit none(external)
          real(dp), intent(inout) :: A(:, :)
         !! Simplex tableau of dimension n+2 x m
          integer(ilp), intent(in) :: nleq, ngeq, neq
         !! Number of constraints of each type.
-         integer(ilp), intent(out) :: icase
+         integer(ilp), intent(out) :: info
         !! Return flag:
-        !!  - icase = 1  : Optimal solution has been found.
-        !!  - icase = 0  : Objective function is unbounded.
-        !!  - icase = -1 : Problem is infeasible.
-         integer(ilp), intent(out) :: iposv(:), izrov(:)
+        !!  - info = 1  : Optimal solution has been found.
+        !!  - info = 0  : Objective function is unbounded.
+        !!  - info = -1 : Problem is infeasible.
+         integer(ilp), intent(out) :: iposv(:)
         !! Book-keeping for the primal and slack variables.
-      end subroutine dense_simplex
+      end subroutine dense_standard_simplex
    end interface simplex
 
-   public :: say_hello
-contains
-   subroutine say_hello
-      print *, "Hello, LightConvex!"
-   end subroutine say_hello
 end module LightConvex
